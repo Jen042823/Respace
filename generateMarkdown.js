d6d0874359c9299d56b1177b2c16673e41f836e3
Jenny Yoon <yoon042823@gmail.com>
2020-05-24 15:45:41 -0400

    initial

---
+++ generateMarkdown.js
@@ -0,0 +1,42 @@
+function generateMarkdown(data) {
+    const fs = require("fs");
+    let filename = "README.md";
+    fs.writeFile(
+        filename,
+`# ${data.title}\n\n
+    [!Github Badge](https://img.shields.io/badge/Github-Profile-blueviolet)](${data.githubprofile})\n\n
+## Table of Contents\n\n
+[Description](#Description)\n
+[Installation](#Installation)\n
+[Usage](#Usage)\n
+[License](#Liense)\n
+[Contributing](#Contributing)\n
+[Tests](#Tests)\n\n
+## Description\n
+${data.description}\n\n
+## Installation\n
+${data.installation}\n\n
+## Usage\n
+${data.usage}\n\n
+## License\n
+${data.licensing}\n
+## Contributing\n
+${data.contribution}\n\n
+## Tests\n
+${data.test}
+`,
+
+        function (err) {
+            if (err) {
+                return console.log(err)
+        } else {
+            console.log("Success! Your README.md has been generated.");
+        }
+        }
+    
+    
+    
+        );
+}
+module.exports = generateMarkdown
+
